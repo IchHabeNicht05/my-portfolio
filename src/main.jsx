f11c4599acf99353cbc640ce1407fa1cb807ad0f
IchHabeNicht05 <martin.habenicht05@gmail.com>
2026-08-23 10:24:41 +0200

    Final verze 1.3.2

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import '@fontsource/inter/400.css';
-import '@fontsource/inter/500.css';
-import '@fontsource/inter/600.css';
-import '@fontsource/inter/700.css';
+
+import '@fontsource/geist/300.css';
+import '@fontsource/geist/400.css';
+import '@fontsource/geist/500.css';
+import '@fontsource/geist/600.css';
+import '@fontsource/geist/700.css';
+import '@fontsource/geist/800.css';
+
+import '@fontsource/jetbrains-mono/400.css';
+import '@fontsource/jetbrains-mono/500.css';
+
 import './index.css'
 import SmoothScroll from './components/ui/SmoothScroll.jsx'
 import App from './App.jsx'
